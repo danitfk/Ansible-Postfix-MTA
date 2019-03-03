c4f57e4e66a6255b9a08c25b42c78e99c1f902f5
Daniel Gordi <danial@gordi.ir>
2019-03-04 01:13:04 +0330

    Add postqueue in go script

--- files/postfix-queue.go
+++ files/postfix-queue.go
@@ -33,10 +33,20 @@ func deferred(w http.ResponseWriter, r *http.Request){
     fmt.Fprint(w,string(out))
 }
 
+func postqueue(w http.ResponseWriter, r *http.Request){
+    cmd := exec.Command("/usr/sbin/postqueue", "-j")
+    out, err := cmd.CombinedOutput()
+    if err != nil {
+           log.Fatalf("cmd.Run() failed with %s\n", err)
+    }
+    fmt.Fprint(w,string(out))
+}
+
 
 func main() {
     http.HandleFunc("/active", active)
     http.HandleFunc("/all", all)
     http.HandleFunc("/deferred", deferred)
+    http.HandleFunc("/postqueue", postqueue)
     http.ListenAndServe(":8080", nil)
 }
